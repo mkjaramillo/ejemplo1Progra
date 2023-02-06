8370977d9456ed6439191824f22c42aa1479628e
mkjaramillo <mkjaramillo@uce.edu.ec>
2023-02-06 05:58:14 +0000

    repaso

--- main.cpp
+++ main.cpp
@@ -3,13 +3,52 @@
 int main(int argc, char** argv){
     int rank;
     int size;
-    int data[100];
-
+    int data[110];
+    int div=110/size 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
-   MPI_Comm_size(MPI_COMM_WORLD, &size); 
-   std::printf("hola soy el rank %d, de un total de %d procesos\n" ,rank,size ); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size); 
 
+   std::printf("hola soy el rank %d, de un total de %d procesos\n" ,rank,size ); 
+    if(rank==3){
+
+        for(int i=0;i<110;i++){
+            data[i]=i;
+        }
+        
+        int part=0;
+        for(int i=0;i<3;i++){
+            MPI_Send(&data[part],div,MPI_INT,i,0,MPI_COMM_WORLD); 
+           part+=div;
+        }
+
+        int sumaParcial=0;
+        for(int i=part;i<110;i++){
+         sumaParcial= sumaParcial+data[i];
+        }
+        
+        int sumaParcial2=0;
+        int suma_total=0;
+        for(int i=0;i<3;i++){
+           MPI_Recv(&sumaParcial2,1,MPI_INT,i,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE); 
+           suma_total=sumaParcial2+suma_total;
+        }
+
+        suma_total= sumaParcial+suma_total;
+        std::printf("la suma total es : %d \n" ,suma_total);
+
+    }else{
+        MPI_Recv(data,div,MPI_INT,MPI_ANY_SOURCE,0,MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int sumaParcial=0;
+        for(int i=0;i<div;i++){
+            sumaParcial=sumaParcial+data[i]; 
+        }
+         std::printf("data[0]: %d ....data[26]:%d \n" ,data[0],data[26]);
+         
+        MPI_Send(&sumaParcial,1,MPI_INT,3,0,MPI_COMM_WORLD); 
+    }
+
+/*
   if(rank==0){ 
     for(int i=0;i<100;i++){
         data[i]=i;
@@ -40,7 +79,7 @@ int main(int argc, char** argv){
     }
     MPI_Send(&sumaParcial,1,MPI_INT,0,0,MPI_COMM_WORLD);     
 
-} 
+} */
    
 
 MPI_Finalize(); 
